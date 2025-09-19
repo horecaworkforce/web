a9864069bdf0c493470def09af79dce7c5b71559
Vinuka Kodituwakku <vinuka.t@pm.me>
2025-09-19 08:12:45 +0530

    feat(phone): changed the phone number

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const FACEBOOK_URL =
 	"https://www.facebook.com/profile.php?id=61573931128141";
 export const WHATSAPP_URL = "https://api.whatsapp.com/send?phone=61402410487";
-export const PHONE_NUMBER = "+61402410487";
+export const PHONE_NUMBER = "+61434382348";
 export const EMAIL = "horecaworkforce@gmail.com";
